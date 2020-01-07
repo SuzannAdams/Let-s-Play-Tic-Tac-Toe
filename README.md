cb2054374653bd7f8c961d4f13af10b02d0674db
SuzannAdams <suzannyee@yahoo.com>
2020-01-06 23:12:49 -0800

    3 by 3 table is set

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Let's Play Tic-Tac-Toe!
 
-Classic strategy play of getting three squares in a row before your opponent to win the game.
+A classic strategy play of getting three squares in a row before your opponent to win the game. Play again if a tie or draw.
---
+++ script.js
@@ -0,0 +1,6 @@
+//create a block grid that is 3 by 3
+referenced https://www.youtube.com/watch?v=P2TcQ3h0ipQ
+//create a choice of two buttons per grid
+//the buttons should be either: X with orange background or O with a green background
+//two players needed to play with the option of playing against the computer
+//endgame message "You win!" or "Tie, play again."

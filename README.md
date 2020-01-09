f7977615082b61dd73721c3e85b910865473e238
SuzannAdams <suzannyee@yahoo.com>
2020-01-09 11:59:11 -0800

    readme 1 on 1/9

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Let's Play Tic-Tac-Toe!
+# Let's Play Tic-Tac-Toe
 
 A classic strategy play of getting three squares in a row before your opponent to win the game. Play again if a tie or draw.
+
+**Projec Preview**
+
+**URL**
+
+**About This Game**
+
+**Technologies Used**
+
+**Wireframes**
+
+**Problems**
+
+**Future Additions**
--- script.js
+++ script.js
@@ -20,8 +20,13 @@ function newGame() {
   //empty out squares
   for (let i = 0; i < square.length; i++) {
     square[i].innerHTML = ''; //going through array and clearing out with ''.
+    square[i].style.backgroundColor = 'white';
   }
   messageDiv.innerText = ''; //to reset the div so the NewGame button will reset
+  for (let i = 0; i < square.length; i++) {
+    square[i].addEventListener('click', clickFn);
+    console.log(square[i]);
+  }
 }
 
 //1st make an eventlistener
@@ -54,7 +59,8 @@ function threeInRow() {
     disableListener();
     console.log(message);
     showMessage();
-    //make a function that checks for a winCombo then stop game and show message 'Winner!'
+    //make a function that checks for a winCombo then stop game and show message 'Winner!' or 'Tie, play again.'
+    winnerColorRed(0, 1, 2);
   } else if (
     square[3].innerHTML !== '' &&
     square[3].innerHTML === square[4].innerHTML &&
@@ -64,6 +70,7 @@ function threeInRow() {
     disableListener();
     console.log(message);
     showMessage();
+    winnerColorRed(3, 4, 5);
   } else if (
     square[6].innerHTML !== '' &&
     square[6].innerHTML === square[7].innerHTML &&
@@ -73,6 +80,7 @@ function threeInRow() {
     disableListener();
     console.log(message);
     showMessage();
+    winnerColorRed(6, 7, 8);
   } else if (
     square[0].innerHTML !== '' &&
     square[0].innerHTML === square[4].innerHTML &&
@@ -82,6 +90,7 @@ function threeInRow() {
     disableListener();
     console.log(message);
     showMessage();
+    winnerColorRed(0, 4, 8);
   } else if (
     square[2].innerHTML !== '' &&
     square[2].innerHTML === square[4].innerHTML &&
@@ -91,6 +100,7 @@ function threeInRow() {
     disableListener();
     console.log(message);
     showMessage();
+    winnerColorRed(2, 4, 6);
   } else if (
     square[0].innerHTML !== '' &&
     square[0].innerHTML === square[3].innerHTML &&
@@ -100,6 +110,7 @@ function threeInRow() {
     disableListener();
     console.log(message);
     showMessage();
+    winnerColorRed(0, 3, 6);
   } else if (
     square[1].innerHTML !== '' &&
     square[1].innerHTML === square[4].innerHTML &&
@@ -109,6 +120,7 @@ function threeInRow() {
     disableListener();
     console.log(message);
     showMessage();
+    winnerColorRed(1, 4, 7);
   } else if (
     square[2].innerHTML !== '' &&
     square[2].innerHTML === square[5].innerHTML &&
@@ -118,13 +130,33 @@ function threeInRow() {
     disableListener();
     console.log(message);
     showMessage();
-  } else if (winCombos === null) {
+    winnerColorRed(2, 5, 8);
+  } else if (
+    square[0].innerHTML !== '' &&
+    square[1].innerHTML !== '' &&
+    square[2].innerHTML !== '' &&
+    square[3].innerHTML !== '' &&
+    square[4].innerHTML !== '' &&
+    square[5].innerHTML !== '' &&
+    square[6].innerHTML !== '' &&
+    square[7].innerHTML !== '' &&
+    square[8].innerHTML !== ''
+  ) {
     message = 'Tie, play again.';
     disableListener();
     console.log(message);
+    showMessage();
     //Jerrica, the last "else if" needs to check if none are empty and would have automatically gone through the no winCombos, then there is a tie. Display message 'Play again.'
   }
 }
+
 function showMessage() {
   messageDiv.innerText = message;
-} //this function created to show 'Winner!' on the board
+} //this function created to show 'Winner!' or 'Tie, play again.' on the board
+
+function winnerColorRed(box1, box2, box3) {
+  square[box1].style.background = 'crimson';
+  square[box2].style.background = 'crimson';
+  square[box3].style.background = 'crimson';
+}
+//this function created to change boxes to red when there is a winner.
